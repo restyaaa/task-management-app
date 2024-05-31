c5b6c823c92bba57136d405f90eb76d7af08de9d
restyaaa <bagasermawan93@gmail.com>
2024-05-31 21:55:15 +0700

    create home page, dark mode, header/navbar

--- task-app/src/pages/header/Header.jsx
+++ task-app/src/pages/header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { Outlet, NavLink } from "react-router-dom";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
@@ -6,14 +6,27 @@ import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
 import "./Header.css";
 import logo from "../../assets/letter-n.png"; // Path yang diperbarui
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+   const [darkMode, setDarkMode] = useState(false);
+
+   const toggleDarkMode = () => {
+     setDarkMode(!darkMode);
+     if (darkMode) {
+       document.body.classList.remove("dark-mode");
+     } else {
+       document.body.classList.add("dark-mode");
+     }
+   };
+
   return (
     <div>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar expand="lg" className="bg-body-tertiary fixed-top">
         <Container fluid className="containerNotic">
-          <Container>
-            <Navbar.Brand href="/">
+          <Container className="container-head">
+            <Navbar.Brand>
               <img
                 src={logo}
                 width="30"
@@ -24,12 +37,19 @@ const Header = () => {
               <span className="fw-bold">Notic</span>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
+            <Navbar.Collapse
+              id="basic-navbar-nav"
+              className="navLink align-items-end"
+            >
               <Nav className="ms-auto">
-                <Nav.Link href="/" className="custom-nav-link">
+                <Nav.Link as={NavLink} to="/" className="custom-nav-link">
                   Home
                 </Nav.Link>
-                <Nav.Link href="/aboutUs" className="custom-nav-link">
+                <Nav.Link
+                  as={NavLink}
+                  to="/aboutUs"
+                  className="custom-nav-link"
+                >
                   About Us
                 </Nav.Link>
                 <NavDropdown
@@ -37,7 +57,7 @@ const Header = () => {
                   id="basic-nav-dropdown"
                   className="custom-nav-link"
                 >
-                  <NavDropdown.Item href="/contactUs">
+                  <NavDropdown.Item as={NavLink} to="/contactUs">
                     Contact Us
                   </NavDropdown.Item>
                   <NavDropdown.Item href="#action/3.2">a</NavDropdown.Item>
@@ -45,8 +65,16 @@ const Header = () => {
                   <NavDropdown.Divider />
                   <NavDropdown.Item href="#action/3.4">c</NavDropdown.Item>
                 </NavDropdown>
-                <Nav.Link href="/login" className="custom-login-link">
+
+                <Nav.Link
+                  as={NavLink}
+                  to="/login"
+                  className="custom-login-link"
+                >
                   Login
+                </Nav.Link>
+                <Nav.Link onClick={toggleDarkMode} className="custom-nav-link">
+                  <FontAwesomeIcon icon={darkMode ? faSun : faMoon} />
                 </Nav.Link>
               </Nav>
             </Navbar.Collapse>
@@ -57,7 +85,6 @@ const Header = () => {
       <div className="content">
         <Outlet />
       </div>
-      
     </div>
   );
 };
