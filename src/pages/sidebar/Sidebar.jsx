eca211ec57adde65c703ae869644aef9f17065c2
restyaaa <bagasermawan93@gmail.com>
2024-06-09 00:30:19 +0700

    (FINAL) Sidebar User

--- src/pages/sidebar/Sidebar.jsx
+++ src/pages/sidebar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { AuthContext } from "../../context/AuthContext";
 import {
   CDBSidebar,
@@ -6,19 +6,30 @@ import {
   CDBSidebarHeader,
   CDBSidebarMenu,
   CDBSidebarMenuItem,
-  CDBListGroup,
-  CDBListGroupItem,
-  CDBContainer
 } from "cdbreact";
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 import "./sidebar.css";
 import profilePicture from "../../assets/restya.jpeg";
 import logo from "../../assets/letter-n.png";
 import { ThemeContext } from "../../context/ThemeContext";
+import { Collapse } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faChevronDown,
+  faChevronRight,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = ({ user }) => {
   const { logout } = useContext(AuthContext);
   const { darkMode } = useContext(ThemeContext);
+  const [openProjects, setOpenProjects] = useState(true); // Initially open
+  const navigate = useNavigate();
+
+  const handleAddProject = () => {
+    navigate("/add-project");
+  };
 
   return (
     <div
@@ -76,18 +87,110 @@ const Sidebar = ({ user }) => {
                 Settings
               </CDBSidebarMenuItem>
             </NavLink>
-            <NavLink
-              to="/project"
-              className={({ isActive }) => (isActive ? "activeClicked" : "")}
-            >
-              <CDBSidebarMenuItem
-                icon="folder-open"
-                style={{ color: darkMode ? "white" : "black" }}
+            <div>
+              <div
+                onClick={() => setOpenProjects(!openProjects)}
+                aria-controls="example-collapse-text"
+                aria-expanded={openProjects}
+                style={{
+                  cursor: "pointer",
+                  display: "flex",
+                  alignItems: "center",
+                }}
               >
-                Project
-              </CDBSidebarMenuItem>
-              
+                <CDBSidebarMenuItem
+                  icon="folder-open"
+                  style={{ color: darkMode ? "white" : "black" }}
+                >
+                  Project
+                </CDBSidebarMenuItem>
+                <FontAwesomeIcon
+                  icon={openProjects ? faChevronDown : faChevronRight}
+                  style={{ marginLeft: "auto", marginRight: "15px" }}
+                />
+              </div>
+              <Collapse in={openProjects}>
+                <div className="collapse-scroll">
+                  <NavLink to="/home" className="dropdown-item">
+                    <CDBSidebarMenuItem
+                      icon="home"
+                      style={{
+                        paddingLeft: "30px",
+                        color: darkMode ? "white" : "black",
+                      }}
+                    >
+                      Home
+                    </CDBSidebarMenuItem>
+                  </NavLink>
+                  <NavLink to="/school" className="dropdown-item">
+                    <CDBSidebarMenuItem
+                      icon="school"
+                      style={{
+                        paddingLeft: "30px",
+                        color: darkMode ? "white" : "black",
+                      }}
+                    >
+                      School
+                    </CDBSidebarMenuItem>
+                  </NavLink>
+                  <NavLink to="/work" className="dropdown-item">
+                    <CDBSidebarMenuItem
+                      icon="briefcase"
+                      style={{
+                        paddingLeft: "30px",
+                        color: darkMode ? "white" : "black",
+                      }}
+                    >
+                      Work
+                    </CDBSidebarMenuItem>
+                  </NavLink>
+                  <NavLink to="/work" className="dropdown-item">
+                    <CDBSidebarMenuItem
+                      icon="briefcase"
+                      style={{
+                        paddingLeft: "30px",
+                        color: darkMode ? "white" : "black",
+                      }}
+                    >
+                      Work
+                    </CDBSidebarMenuItem>
+                  </NavLink>
+                  <NavLink to="/work" className="dropdown-item">
+                    <CDBSidebarMenuItem
+                      icon="briefcase"
+                      style={{
+                        paddingLeft: "30px",
+                        color: darkMode ? "white" : "black",
+                      }}
+                    >
+                      Work
+                    </CDBSidebarMenuItem>
+                  </NavLink>
+                  <div
+                    onClick={handleAddProject}
+                    className="text-decoration-none dropdown-item"
+                    style={{
+                      paddingLeft: "40px",
+                      cursor: "pointer",
+                      color: "#0d6efd",
+                    }}
+                  ></div>
+                </div>
+              </Collapse>
+              <hr />
+            </div>
+            <NavLink>
+              <CDBSidebarMenuItem
+              className="add-new-project"
+              icon="plus-square"
+              style={{
+                color: darkMode ? "white" : "black",
+              }}
+            >
+              Add new project
+            </CDBSidebarMenuItem>
             </NavLink>
+            
             <div
               style={{
                 color: "red",
